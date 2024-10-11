308442c98f73e7c22ea6c1508c2e1ed0241e4337
Mardie328 <tolayinka328@gmail.com>
2024-10-11 14:06:44 +0100

    Feat: task 6 updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return result.value; // This will have the shape: { firstName: string; lastName: string; }
-      }
-      return result.reason.message; // This will be a string from the rejection.
-    });
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
 }
